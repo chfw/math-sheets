e0b60dd7325f2c75366cab09c13e45c63d1e7818
chfw <wangc_2011@hotmail.com>
2020-04-20 09:36:32 +0100

    :lipstick: add marks place holder

--- reception/a_op_b/make.sh
+++ reception/a_op_b/make.sh
@@ -1,5 +1,5 @@
 python math_work.py
-moban -e jj2=jinja2_time.TimeExtension -d name=$1 -c data.yml -t math_sheet.html.jj2 -o tmp.html > /dev/null
+moban -td ../../templates -e jj2=jinja2_time.TimeExtension -d name=$1 -c data.yml -t math_sheet.html.jj2 -o tmp.html > /dev/null
 weasyprint tmp.html math-sheet.pdf
 rm tmp.html data.yml
 echo 'math-sheet.pdf is generated'
