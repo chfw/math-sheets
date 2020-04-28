7ebd5180b6fa163db13710a5fa7686384c7c53fb
chfw <wangc_2011@hotmail.com>
2020-04-28 13:51:36 +0100

    :sparkles: a + __ = c

---
+++ reception/a_op_x_op_c/make.sh
@@ -0,0 +1,5 @@
+python math_work.py
+moban -td ../../templates -e jj2=jinja2_time.TimeExtension -d name=$1 -c data.yml -t math_sheet.html.jj2 -o tmp.html > /dev/null
+weasyprint tmp.html math-sheet.pdf
+rm tmp.html data.yml
+echo 'math-sheet.pdf is generated'
--- reception/a_op_x_op_c/math_work.py
+++ reception/a_op_x_op_c/math_work.py
@@ -11,23 +11,16 @@ def main():
     rows = []
     for row in range(COUNT):
         index = random.randint(0, 1)
-        index2 = random.randint(0, 1)
-        a = random.randint(2, 7)
-        b = random.randint(2, 7)
-        c = random.randint(2, 7)
+        a = random.randint(0, 10)
+        b = random.randint(0, 10)
         op = OPS[index]
-        op2 = OPS[index2]
         if op == MINUS:
             if a < b:
                 a, b = b, a
-        if op2 == MINUS:
-            if op == MINUS:
-                if (a-b) < c:
-                    op2 = PLUS
-            elif op == PLUS:
-                if (a+b) < c:
-                    op2 = PLUS
-        rows.append({'a': a, 'b': b, 'op': op, 'c': c, 'op2': op2})
+            c = a - b
+        else:
+            c = a + b
+        rows.append({'a': a, 'c': c, 'op': op})
 
     content = yaml.dump({'rows': rows}, default_flow_style=False)
     with open('data.yml', 'w') as f:
